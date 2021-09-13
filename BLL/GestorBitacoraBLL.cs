ddc7a962351090dd6fab099361fd2fb1cf4b0b28
Otto <gonott@gmail.com>
2021-09-13 00:12:32 -0300

    Agregación de Metodos en Gestor de Bitacora capa BLL

--- BLL/GestorBitacoraBLL.cs
+++ BLL/GestorBitacoraBLL.cs
@@ -4,15 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DAL;
+using SERVICIOS;
 
 namespace BLL
 {
     public class GestorBitacoraBLL
     {
 
+        GestorBitacoraDAL mapper = new GestorBitacoraDAL();
 
-
-
+        public List<string> LeerBitacora()
+        {
+            
+            return mapper.LeerBitacora();
+            
+        }
+            
+        
+        public void Grabar(string evento)
+        {
+            mapper.Grabar(evento, Sesion.ObtenerInstancia.EsteUsuario.IdUsuario);
+        }
 
     }
 }
