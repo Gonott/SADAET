0f2a1f79ad546a4e9a973a33d9a4673f63e857aa
Otto <gonott@gmail.com>
2021-11-24 12:01:11 -0300

    Agregación de Serializacion en Capa SERVICIOS

---
+++ SERVICIOS/ExepciónSerializada.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SERVICIOS
+{
+    /// <summary>
+    /// Sirve para encapsular la excepción a serializar, 
+    /// cada vez que se genera una exepción no controlada, cae aca y esto se serializa.
+    /// </summary>
+    
+    [Serializable]
+    public class ExepcionSerializada
+    {
+
+        public string Tipo { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+        public string Message { get; set; }
+        public string StackTrace { get; set; }
+
+        public ExepcionSerializada() 
+        { 
+            this.TimeStamp = DateTime.Now;
+        }
+
+        public ExepcionSerializada(string Message) : this() 
+        {
+            this.Message = Message;
+        }
+
+        public ExepcionSerializada(System.Exception ex) : this(ex.Message) 
+        {
+            this.StackTrace = ex.StackTrace; 
+            this.Tipo = ex.GetType().ToString(); 
+        }
+        public override string ToString() { return this.Message + this.StackTrace; }
+
+
+
+    }
+
+
+
+}
---
+++ SERVICIOS/Serializador.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+using System.Xml;
+
+
+
+
+namespace SERVICIOS
+{
+    /// <summary>
+    /// Esta clase es la que va a cumplir con A02 Serialización, recibe los objetos que se desean serializar y llama a un generador mail
+   
+    /// </summary>
+    public class Serializador
+    {
+
+
+        public void Serializar(Exception ex)
+        {
+            ExepcionSerializada excep = new ExepcionSerializada(ex);
+
+
+            //esto para tirarlo a un path Filestream
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            System.IO.FileStream file = System.IO.File.Create(path);
+
+            //esto para memorystream
+            System.IO.MemoryStream stream = new System.IO.MemoryStream();
+            //luego hay que mandarlo por mail de alguna manera
+
+            //crear el serializador
+            XmlSerializer x = new XmlSerializer(excep.GetType());
+
+
+
+            
+            using (XmlWriter writer = XmlWriter.Create("ExcepcionSerializada.xml"))
+            {
+                x.Serialize(writer, excep);
+            }
+           
+            
+            //XmlTextWriter xmltextWriter = new XmlTextWriter(stream, System.Text.Encoding.UTF8);
+            //x.Serialize(xmltextWriter, excep);
+            //xmltextWriter.Close();
+           
+            //Process.Start("Outlook.exe /a " + path);
+        }
+
+
+
+
+
+    }
+}
