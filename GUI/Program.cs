df2ca8aeec2bb97876f1db65a26a96394533389c
Otto <gonott@gmail.com>
2021-11-26 01:41:43 -0300

    Advertencia de envio por outlook de archivo serializado + message box de ruta de archivo serializado.

--- GUI/Program.cs
+++ GUI/Program.cs
@@ -29,7 +29,7 @@ namespace GUI
         static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
         {
             // Hacer Algo con la exepción
-            DialogResult result = MessageBox.Show("Hay una exepción no manejada, ¿Desea enviarla por correo al desarrollador? ", e.Exception.Message, MessageBoxButtons.YesNo);
+            DialogResult result = MessageBox.Show("Hay una exepción no manejada, ¿Desea enviarla por correo al desarrollador? Es necesario que tenga Microsoft Outlook Instalado.", e.Exception.Message, MessageBoxButtons.YesNo);
             if (result == DialogResult.Yes)
             {
                 Serializador serializador = new Serializador();
@@ -43,12 +43,7 @@ namespace GUI
         static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
         {
             // Hacer Algo con la exepción
-            DialogResult result = MessageBox.Show("Hay una exepción no manejada, ¿Desea enviarla por correo al desarrollador? ", (e.ExceptionObject as Exception).Message, MessageBoxButtons.YesNo);
-            if (result == DialogResult.Yes)
-            {
-                Serializador serializador = new Serializador();
-                serializador.Serializar(e.ExceptionObject as Exception);
-            }
+            MessageBox.Show("No se pudo enviar el correo, la excepción se guardó en: " + Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
 
         }
     }
