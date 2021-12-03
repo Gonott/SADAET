cf81b1c39779cc22161fb05b3c425e2c6f8051a1
Otto <gonott@gmail.com>
2021-12-02 21:55:13 -0300

    Manejo de excepciones no manejadas.

--- GUI/Program.cs
+++ GUI/Program.cs
@@ -51,9 +51,23 @@ namespace GUI
 
         static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
         {
-            
+
             // Hacer Algo con la exepción
-            MessageBox.Show("No se pudo enviar el correo, la excepción se guardó en: " + Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+            DialogResult result = MessageBox.Show("Hay una exepción no manejada, ¿Desea enviarla por correo al desarrollador? Es necesario que tenga Microsoft Outlook Instalado.", e.Exception.Message, MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                try
+                {
+                    Serializador serializador = new Serializador();
+                    serializador.Serializar(e.ExceptionObject as Exception);
+                }
+                catch (Exception)
+                {
+
+                    MessageBox.Show("No se pudo enviar el correo, la excepción se guardó en: " + Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+                }
+
+            }
 
         }
     }
